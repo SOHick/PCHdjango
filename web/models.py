1819ac8ba3c33ed51cfd9099c4485053bf22340b
SOHick <shnikitaprog@mail.ru>
2023-11-27 21:08:34 +0300

    + migrations

--- web/models.py
+++ web/models.py
@@ -5,17 +5,18 @@ User = get_user_model()
 
 
 class TimeSlotTag(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=256)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class TimeSlot(models.Model):
-    title = models.ChoiceField(max_length=256)
+    title = models.CharField(max_length=256)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     is_realtime = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     tags = models.ManyToManyField(TimeSlotTag)
+    image = models.ImageField(upload_to='time_slots/',null=True,blank=True)
 
 
 class Holiday(models.Model):
