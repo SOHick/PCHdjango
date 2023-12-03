15ed83b8f34c6d3dd38fc45a040ef0f504bacae7
SOHick <shnikitaprog@mail.ru>
2023-12-03 23:49:19 +0300

    + test

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from web.views import main_view, registration_view, auth_view, logout_view
-
+# sadas
 urlpatterns = [
     path("", main_view, name="main"),
     path("registration/", registration_view, name="registration"),
