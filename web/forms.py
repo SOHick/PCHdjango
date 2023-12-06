d1a3f48349d8a68e34421492411ee9f6c8740c76
SOHick <shnikitaprog@mail.ru>
2023-12-07 00:31:38 +0300

    + add picture

--- web/forms.py
+++ web/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from django.contrib.auth import get_user_model
 
+from web.models import TimeSlot
+
 User = get_user_model()
+
+
 class RegistrationForm(forms.ModelForm):
     password2 = forms.CharField(widget=forms.PasswordInput())
 
@@ -11,12 +15,30 @@ class RegistrationForm(forms.ModelForm):
             self.add_error("password", "Пароли не совпадают")
         return cleaned_data
 
-
     class Meta:
         model = User
         fields = ("email", "username", "password", "password2")
+
 
 class AuthForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput())
 
+
+class TimeSlotForm(forms.ModelForm):
+
+    def save(self, commit=True):
+        self.instance.user = self.initial['user']
+        return super().save(commit)
+
+    class Meta:
+        model = TimeSlot
+        fields = ("title", "start_date", "end_date", "image")
+        widgets = {
+            "start_date": forms.DateTimeInput(
+                attrs={"type": "datetime-local"}, format='%Y-%m-%dT%H:%M'),
+
+            "end_date": forms.DateTimeInput(
+                attrs={"type": "datetime-local"}, format='%Y-%m-%dT%H:%M'),
+
+        }
--- web/views.py
+++ web/views.py
@@ -1,18 +1,16 @@
-from datetime import datetime
-
 from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.shortcuts import render, redirect
 
-from web.forms import RegistrationForm, AuthForm
+from web.forms import RegistrationForm, AuthForm, TimeSlotForm
+from web.models import TimeSlot
 
 User = get_user_model()
 
 
 def main_view(request):
-    year = datetime.now().year
+    timeslots = TimeSlot.objects.all()
     return render(request, "web/main.html", {
-        "year": year,
-
+        'timeslots': timeslots
     })
 
 
@@ -43,12 +41,24 @@ def auth_view(request):
         if form.is_valid():
             user = authenticate(**form.cleaned_data)
             if user is None:
-                form.add_error(None,"Введены неверные данные")
+                form.add_error(None, "Введены неверные данные")
             else:
-                login(request,user)
+                login(request, user)
                 return redirect("main")
     return render(request, "web/auth.html", {"form": form})
+
 
 def logout_view(request):
     logout(request)
     return redirect("main")
+
+
+def time_slot_edit_view(request, id=None):
+    timeslot = TimeSlot.objects.get(id=id) if id is not None else None
+    form = TimeSlotForm(instance=timeslot)
+    if request.method == 'POST':
+        form = TimeSlotForm(data=request.POST, files=request.FILES, instance=timeslot, initial={"user": request.user})
+        if form.is_valid():
+            form.save()
+            return redirect("main")
+    return render(request, "web/time_slot_form.html", {"form": form})
